0abcdfb78393c0a38566a42ad95bd8b76f73ebed
Vijayendra Pagare <vijayendrapagare05@gmail.com>
2019-07-23 00:24:06 +0530

    version 0.1

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var mysql = require("mysql");
+var path = require("path");
+var bodyparser = require("body-parser");
+
+//var flash = require("connect-flash");
+var app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine","ejs");
+
+
+app.use(bodyparser());
+app.use(express.static(path.join(__dirname,"static")));
+
+const port=process.env.PORT || 3000
+app.listen(port,function()
+{
+	console.log("listen to port 3000");
+});
+
+
+app.get("/",function(req,res)
+{
+	res.render("imageupload");
+})
+
---
+++ static/imageupload.js
@@ -0,0 +1,44 @@
+function readURL(input)
+{
+	console.log(input.files);
+	if (input.files && input.files[0])
+	{
+	    var reader = new FileReader();
+
+	    reader.onload = function (e)
+	    {
+	        $('#pic')
+	            .attr('src', e.target.result);
+	    };
+
+	    reader.readAsDataURL(input.files[0]);
+	}
+}
+$(document).ready(function()
+{
+	$("#predict").click(function()
+	{
+		predict();
+	})
+});
+
+
+async function predict()
+{
+	console.log("Hello");
+	await stackml.init({'accessKeyId': 'dca3d99c867b87cc2263f7c07987ccfc'});
+	// load face detection model
+	const model = await stackml.faceDetection(function callbackLoad()
+	{
+	    console.log('callback after face detection model is loaded!');
+	});
+
+	// make prediction with the image
+	model.detect(document.getElementById('pic'),function callbackPredict(err, results)
+	{
+	    console.log(results);
+
+	    // draw output keypoints in the image
+	    model.draw(document.getElementById('pic'), results);
+	});	
+}
